c2d1f733aeeedf153920bcc834be1fc9f6e9cd7f
Umang Bhatia <Umang Bhatia@DESKTOP-TN810R1>
2018-12-15 12:08:42 +0530

    Added data for CompareWindow title

--- SampleSeleniumFramework/src/test/java/com/utility/Utility.java
+++ SampleSeleniumFramework/src/test/java/com/utility/Utility.java
@@ -5,5 +5,7 @@ public class Utility {
 	public static final int IMPLICIT_WAIT = 10;
 	public static final String QTY_UPDATE_ERROR = "* The requested quantity for \"Sony Xperia\" is not available.";
 	public static final String CART_EMPTY_MSG = "You have no items in your shopping cart.";
+	public static final String COMPARE_WINDOW_TITLE = "Products Comparison List - Magento Commerce";
+	
 	
 }
